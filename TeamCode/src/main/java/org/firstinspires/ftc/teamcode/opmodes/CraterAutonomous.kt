b05d36d12bfe3a8c0966453d37c664821e42284d
dansman805 <dansman805@gmail.com>
2018-12-29 18:30:50 -0500

    Wrote new CraterAutonomous that is 100% command based

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CraterAutonomous.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CraterAutonomous.kt
@@ -1,86 +1,25 @@
 package org.firstinspires.ftc.teamcode.opmodes
 
-import com.qualcomm.hardware.bosch.BNO055IMU
-import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor
-import com.qualcomm.hardware.rev.Rev2mDistanceSensor
+import com.jdroids.robotlib.command.SequentialCommandGroup
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
-import com.qualcomm.robotcore.eventloop.opmode.Disabled
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-import com.qualcomm.robotcore.hardware.*
-//import org.firstinspires.ftc.teamcode.SamplingVision
-import org.firstinspires.ftc.teamcode.Util
-import org.firstinspires.ftc.teamcode.Util.toRadians
-import org.firstinspires.ftc.teamcode.pathplanning.*
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
+import org.firstinspires.ftc.teamcode.robot.commands.Land
+import org.firstinspires.ftc.teamcode.robot.commands.TravelFeet
+import org.firstinspires.ftc.teamcode.robot.commands.TurnToAngle
+import org.firstinspires.ftc.teamcode.robot.commands.TurnToGold
 
-@Disabled
-@Autonomous(name="Crater Auto old")
-class CraterAutonomous : LinearOpMode() {
-    private val leftMotor by lazy {hardwareMap!!.get(DcMotorEx::class.java, "left")}
-    private val rightMotor by lazy {hardwareMap!!.get(DcMotorEx::class.java, "right")}
-    private val imu by lazy {hardwareMap!!.get(BNO055IMU::class.java, "imu")}
-
-    private val hangMotor1 by lazy {hardwareMap!!.get(DcMotorEx::class.java, "hang1")}
-    private val hangMotor2 by lazy {hardwareMap!!.get(DcMotorEx::class.java, "hang2")}
-    private val hangServo by lazy {hardwareMap!!.get(Servo::class.java, "hangServo")}
-
-    private val markerServo by lazy {hardwareMap!!.get(Servo::class.java, "depotServo")}
-
-    private val hangSensor by lazy {
-        hardwareMap!!.get(DigitalChannel::class.java, "hangSensor")}
-
-    private val frontUltrasonic
-            by lazy {hardwareMap!!.get(ModernRoboticsI2cRangeSensor::class.java, "rangeFront")}
-
-    override fun runOpMode() {
-        leftMotor.direction = DcMotorSimple.Direction.REVERSE
-
-        leftMotor.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
-        rightMotor.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
-
-        hangMotor2.direction = DcMotorSimple.Direction.REVERSE
-
-        waitForStart()
-
-        Util.land(this, hangMotor1, hangMotor2, hangSensor)
-
-        leftMotor.mode = DcMotor.RunMode.RUN_USING_ENCODER
-        rightMotor.mode = DcMotor.RunMode.RUN_USING_ENCODER
-
-        leftMotor.power = -1.0
-        rightMotor.power = -1.0
-
-        sleep(300)
-
-        leftMotor.power = 0.0
-        rightMotor.power = 0.0
-
-        sleep(100)
-
-        sleep(200)
-
-        Util.initializeIMU(imu)
-
-        Util.moveFeet(2.5, this, leftMotor, rightMotor)
-
-        sleep(100)
-
-        Util.turnTime(1500, -0.3, this, leftMotor, rightMotor)
-
-        Util.moveFeet(3.3, this, leftMotor, rightMotor)
-
-        Util.turnTime(800, -0.3, this, leftMotor, rightMotor)
-
-        Util.moveFeet(3.9, this, leftMotor, rightMotor)
-
-        Util.turnTime(600, 0.3, this, leftMotor, rightMotor)
-
-        Util.setMarkerState(Util.MarkerState.OPENED, markerServo)
-
-        Util.turnTime(500, -0.3, this, leftMotor, rightMotor)
-
-        Util.moveFeet(-8.5, this, leftMotor, rightMotor)
-
-        sleep(100)
-
-    }
-}
+@Autonomous(name="Crater Auto")
+class CraterAutonomous : CommandOpMode(
+    SequentialCommandGroup(
+        Land(),
+        TravelFeet(0.1),
+        TurnToGold(),
+        TravelFeet(3.2),
+        TravelFeet(-1.8),
+        TurnToAngle(AngleUnit.DEGREES, 135.0),
+        TravelFeet(3.0),
+        TurnToAngle(AngleUnit.DEGREES,90.0),
+        TravelFeet(5.0),
+        TravelFeet(-6.0)
+    )
+)
